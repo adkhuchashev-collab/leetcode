bb9b9c39003ee575559d07413237f8cfc0e531be
Alfred <akhuchashev@bellintegrator.ru>
2026-05-23 01:06:12 +0300

    Binary search

---
+++ binary_search.go
@@ -0,0 +1,17 @@
+package main
+
+func search(nums []int, target int) int {
+	start, end := 0, len(nums)-1
+	for start <= end {
+		mid := (start + end) / 2
+		switch {
+		case nums[mid] == target:
+			return mid
+		case nums[mid] > target:
+			end = mid - 1
+		case nums[mid] < target:
+			start = mid + 1
+		}
+	}
+	return -1
+}
